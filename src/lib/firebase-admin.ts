025a3d33871adfa37e9501df4a2130b2eb0b6ad2
Brandon Beierle <bbeierle21@gmail.com>
2025-09-18 04:21:12 +0000

    Try fixing this error: `Runtime Error: Error: The default Firebase app d

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,25 +4,26 @@ import { config } from 'dotenv';
 config();
 
 if (!admin.apps.length) {
-  try {
-    // When deployed, this will use the service account from the environment
-    if (process.env.NODE_ENV === 'production' && process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-        admin.initializeApp({
-            credential: admin.credential.applicationDefault(),
-        });
-    } else {
-        // For local development, use the service account key from .env
-        const serviceAccount = {
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-        });
-    }
-  } catch (error: any) {
-    console.error('Firebase admin initialization error', error.stack);
+  // When deployed, this will use the service account from the environment
+  if (process.env.NODE_ENV === 'production' && process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+      admin.initializeApp({
+          credential: admin.credential.applicationDefault(),
+      });
+  } else {
+      // For local development, use the service account key from .env
+      const serviceAccount = {
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      } as admin.ServiceAccount;
+
+      if (!serviceAccount.projectId || !serviceAccount.clientEmail || !serviceAccount.privateKey) {
+        throw new Error('Firebase Admin SDK Service Account credentials are not defined in .env file. Please add FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, and FIREBASE_PRIVATE_KEY.');
+      }
+      
+      admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount),
+      });
   }
 }
 
