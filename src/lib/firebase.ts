3b117b0886715c8fc664211482492415aa517f67
Brandon Beierle <bbeierle21@gmail.com>
2025-09-19 04:20:30 +0000

    I thought a fire base project was created for this already.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
   projectId: 'studio-5182661874-6b3b7',
   appId: '1:382448659100:web:74c9f9327ef1fd6a175b59',
-  storageBucket: 'studio-5182661874-6b3b7.firebasestorage.app',
+  storageBucket: 'studio-5182661874-6b3b7.appspot.com',
   apiKey: 'AIzaSyAZ-_rV2KMDipKVJV_E9b2rKzvPXNJyuaI',
   authDomain: 'studio-5182661874-6b3b7.firebaseapp.com',
   messagingSenderId: '382448659100',
